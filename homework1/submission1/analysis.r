bc8742ddd94d3b12cb65544ec146fe49216b3e95
Tammy <133098831+tdangi@users.noreply.github.com>
2026-01-23 19:03:22 -0500

    idek whats happening

--- homework1/submission1/analysis.r
+++ homework1/submission1/analysis.r
@@ -3,12 +3,12 @@ library(tidyverse)
 
 
 ## q1 provide a table under each plan type ##################
-str(plan.year)
-table(plan.year$plan_type) #i forgot how to do the headers and name the column 2018...
+str(final.plans)
+table(final.plans$plan_type) #i forgot how to do the headers and name the column 2018...
 
 #q2 ##########
-unique(plan.year$plan_type) #find all the unique names for plan type
-plan.year_q2 <- plan.year %>% filter(
+unique(final.plans$plan_type) #find all the unique names for plan type
+finalplan.year_q2 <- final.plans %>% filter(
                                 !str_detect(
                                     plan_type,
                                     regex(
@@ -18,11 +18,25 @@ plan.year_q2 <- plan.year %>% filter(
     )
   )
 #check that I removed: "HCPP - 1833 Cost" | "1876 Cost" | "Medicare-Medicaid Plan HMO/HMOPOS" | "Employer/Union Only Direct Contract PDP"
-unique(plan.year_q2$plan_type) 
+unique(finalplan.year_q2$plan_type) 
 
 #create new table
-table(plan.year_q2$plan_type)
+table(finalplan.year_q2$plan_type)
 
 
 ####### q3 ############
-#NEED TO DO THE DATA FOR THE service area files
+service_merge_clean <- service_merge %>%
+  rename(
+    plan_type = plan_type.y
+  )
+
+# average enrollment by type
+avg_enroll_by_plan_type <- service_merge_clean %>%
+  group_by(plan_type) %>%
+  summarize(
+    avg_enrollment = mean(avg_enrollment, na.rm = TRUE),
+    n_plans = n(),
+    .groups = "drop"
+  )
+avg_enroll_by_plan_type 
+
